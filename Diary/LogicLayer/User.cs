bc3a873769b73e30291aab658fd0701a9ba6d5b9
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2023-03-29 08:37:39 +0200

    Ajout classe utilisateur

--- Diary/LogicLayer/User.cs
+++ Diary/LogicLayer/User.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace LogicLayer
 {
+    /// <summary>
+    /// Utilisateur
+    /// </summary>
     public class User
     {
+        private string login;
+        private string name;
+        private UserRoles role;
+        private string password;
+        private int id;
+
+        /// <summary>
+        /// Login de l'utilisateur
+        /// </summary>
+        public string Login { get => login; set => login = value; }
+
+        /// <summary>
+        /// Nom de l'utilisateur
+        /// </summary>
+        public string Name { get => name; set => name = value; }
+
+        /// <summary>
+        /// Rôle de l'utilisateur
+        /// </summary>
+        public UserRoles Role { get => role; set => role = value; }
+
+        /// <summary>
+        /// Mot de passe de l'utilisateur
+        /// </summary>
+        public string Password { get => password; set => password = value; }
+
+        /// <summary>
+        /// ID de l'utilisateur
+        /// </summary>
+        public int Id { get => id; set => id = value; }
     }
 }
