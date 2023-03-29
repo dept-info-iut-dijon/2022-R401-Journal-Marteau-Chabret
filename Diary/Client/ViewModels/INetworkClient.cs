5fb12320eb6b63840f295c1cc8b0f352ad259cfb
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2023-03-29 08:43:18 +0200

    Finalisation Client

--- Diary/Client/ViewModels/INetworkClient.cs
+++ Diary/Client/ViewModels/INetworkClient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LogicLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Diary/LogicLayer/Teacher.cs
+++ Diary/LogicLayer/Teacher.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace LogicLayer
 {
-    public class Teacher
+    public class Teacher : User
     {
+        private List<Student> students;
     }
 }
