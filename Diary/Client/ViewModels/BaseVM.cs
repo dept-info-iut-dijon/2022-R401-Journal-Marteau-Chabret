d7c994e7750b7ce8828683275968bf60172abe92
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2023-03-29 08:35:54 +0200

    Ajout partie client

--- Diary/Client/ViewModels/BaseVM.cs
+++ Diary/Client/ViewModels/BaseVM.cs
@@ -7,8 +7,13 @@ using System.Threading.Tasks;
 
 namespace Client.ViewModels
 {
+    /// <summary>
+    /// Base
+    /// </summary>
     internal class BaseVM : INotifyPropertyChanged
     {
+        #region INOTIFY
         public event PropertyChangedEventHandler? PropertyChanged;
+        #endregion
     }
 }
---
+++ Diary/Client/ViewModels/INetworkClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.ViewModels
+{
+    /// <summary>
+    /// INetwork client
+    /// </summary>
+    public interface INetworkClient
+    {
+        /// <summary>
+        /// Récupère le journal
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public Task<Diary> GetDiary(Student user);
+
+        /// <summary>
+        /// Lis les catégories
+        /// </summary>
+        /// <returns> Liste de catégories </returns>
+        public Task<Categories> ReadCategories();
+
+        /// <summary>
+        /// Ajoute une entrée
+        /// </summary>
+        /// <param name="newEntry"></param>
+        public void AddEntry(Entry newEntry);
+    }
+}
