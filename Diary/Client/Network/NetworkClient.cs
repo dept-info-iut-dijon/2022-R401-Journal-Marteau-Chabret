6616666b0d918238e44c8b826afc5c3389556ced
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2023-03-29 08:25:59 +0200

    Partie Client

---
+++ Diary/Client/Network/NetworkClient.cs
@@ -0,0 +1,27 @@
+﻿using Client.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Network
+{
+    public class NetworkClient : INetworkClientcs
+    {
+        public void AddEntry(string newEntry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetDiary(string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadCategories()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
