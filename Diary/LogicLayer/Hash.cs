4e67e5cc178ebb3406c69b67c65f201c52fc4342
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2023-04-27 09:15:21 +0200

    Ajout classe - Hash pour hasher un string en sha256

---
+++ Diary/LogicLayer/Hash.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    /// <summary>
+    /// Classe qui permet de hash un string
+    /// </summary>
+    public class Hash
+    {
+        /// <summary>
+        /// Constructeur
+        /// </summary>
+        public Hash() { }
+
+        /// <summary>
+        /// hash un texte en sha256
+        /// </summary>
+        /// <param name="text"> text à hash </param>
+        /// <returns> le nouveau texte à hashé </returns>
+        public string HashStringToSHA256(string text)
+        {
+            // Transforme le string en byte
+            byte[] tab = Encoding.UTF8.GetBytes(text);
+
+            // Hash le tableaud de byte
+            byte[] hashTab = SHA256.HashData(tab);
+
+            // Converti en chaine héxadécimale
+            return BitConverter.ToString(tab).Replace("-", "");
+        }
+    }
+}
